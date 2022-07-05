75e3dd1b3804293e4033ac54a3700609e9db90a1
MateuszChmiest <mateusz_269@icloud.com>
2022-07-05 17:02:37 +0200

    adding components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				{/* <Route path='/Login' element={<Login />} />
+				<Route path='/Register' element={<Register />} /> */}
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Element } from "react-scroll";
+import HomeHeader from "./HomeHeader";
+import HomeThreeColumns from "./HomeThreeColumns";
+import HomeSimpleSteps from "./HomeSimpleSteps";
+import HomeAboutUs from "./HomeAboutUs";
+import HomeWhoWeHelp from "./HomeWhoWeHelp";
+import HomeContact from "./HomeContact";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	return (
+		<>
+			<Element name='HomeHeader'>
+				<HomeHeader />
+			</Element>
+			<Element name="HomeThreeColumns">
+      <HomeThreeColumns />
+      </Element>
+			<Element name="HomeSimpleSteps">
+        <HomeSimpleSteps/>
+      </Element>
+			<Element name="HomeAboutUs">
+        <HomeAboutUs/>
+      </Element>
+			<Element name="HomeWhoWeHelp">
+        <HomeWhoWeHelp/>
+      </Element>
+			<Element name="HomeContact">
+        <HomeContact/>
+      </Element>
+		</>
+	);
+};
 
 export default Home;
